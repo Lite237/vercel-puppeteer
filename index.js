426db092dc70fa4aafdf7feffc803da89c2da386
LexTech <ryanvalentin288@gmail.com>
2023-07-03 23:27:43 +0100

    Upadat index.js

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
     puppeteer = require("puppeteer");
 }
 
-let options = {};
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-        defaultViewport: chrome.defaultViewport,
-        executablePath: await chrome.executablePath,
-        headless: true,
-        ignoreHTTPSErrors: true,
-    };
-}
-
 app.get("/api", async (req, res) => {
+    let options = {};
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+            args: [
+                ...chrome.args,
+                "--hide-scrollbars",
+                "--disable-web-security",
+            ],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
+    }
     try {
         let browser = await puppeteer.launch(options);
 
@@ -37,6 +40,21 @@ app.get("/api", async (req, res) => {
 });
 
 app.get("/screenshot", async (req, res) => {
+    let options = {};
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+            args: [
+                ...chrome.args,
+                "--hide-scrollbars",
+                "--disable-web-security",
+            ],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
+    }
     try {
         const url = new URL(req.url);
         const link = url.searchParams.get("url");
