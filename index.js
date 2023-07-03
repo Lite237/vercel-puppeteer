ccf915efc063396051f472d47a4bf1a7e6969080
LexTech <ryanvalentin288@gmail.com>
2023-07-03 23:05:06 +0100

    Added screenshot route & README.md file

--- index.js
+++ index.js
@@ -1,42 +1,66 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
 
 let chrome = {};
 let puppeteer;
 
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
+    chrome = require("chrome-aws-lambda");
+    puppeteer = require("puppeteer-core");
 } else {
-  puppeteer = require("puppeteer");
+    puppeteer = require("puppeteer");
+}
+
+let options = {};
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+    options = {
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+    };
 }
 
 app.get("/api", async (req, res) => {
-  let options = {};
+    try {
+        let browser = await puppeteer.launch(options);
 
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
+        let page = await browser.newPage();
+        await page.goto("https://www.google.com");
+        res.send(await page.title());
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
+});
 
-  try {
-    let browser = await puppeteer.launch(options);
+app.get("/screenshot", async (req, res) => {
+    try {
+        const url = new URL(req.url);
+        const link = url.searchParams.get("url");
 
-    let page = await browser.newPage();
-    await page.goto("https://www.google.com");
-    res.send(await page.title());
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
+        const browser = await puppeteer.launch(options);
+
+        const page = await browser.newPage();
+        await page.goto(link);
+
+        await puppeteer.screenchot({
+            path: "./file.png",
+            fullPage: true,
+        });
+
+        res.sendFile("./file.png");
+    } catch (error) {
+        res.status(400).json({
+            message: "Invalid url parameter",
+        });
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+    console.log("Server started");
 });
 
 module.exports = app;
